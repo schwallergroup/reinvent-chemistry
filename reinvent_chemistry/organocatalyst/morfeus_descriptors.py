1e171c115167169cce8d57d1b1231be36b241b6d
GuoJeff <jeff.guo@outlook.com>
2022-12-26 14:05:44 +0100

    added a try,except block in case xtb --opt does not converge, in which case the optimized geometry file is not outputted

--- reinvent_chemistry/organocatalyst/morfeus_descriptors.py
+++ reinvent_chemistry/organocatalyst/morfeus_descriptors.py
@@ -15,61 +15,89 @@ class MorfeusDescriptors:
         self._geometry_optimizer = GeometryOptimizer()
 
     def ionization_potential(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return xtb.get_ip(corrected=True)
+            return xtb.get_ip(corrected=True)
+
+        except FileNotFoundError:
+            return 0.0
 
     def electron_affinity(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return xtb.get_ea(corrected=True)
+            return xtb.get_ea(corrected=True)
+
+        except FileNotFoundError:
+            return 0.0
 
     def homo(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return xtb.get_homo()
+            return xtb.get_homo()
+
+        except FileNotFoundError:
+            return 0.0
 
     def lumo(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return xtb.get_lumo()
+            return xtb.get_lumo()
+
+        except FileNotFoundError:
+            return 0.0
 
     def dipole(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        dipoles = xtb.get_dipole()
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            dipoles = xtb.get_dipole()
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return np.sqrt(np.array(dipoles).dot(np.array(dipoles).T))
+            return np.sqrt(np.array(dipoles).dot(np.array(dipoles).T))
+
+        except FileNotFoundError:
+            return 0.0
 
     def global_electrophilicity(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return xtb.get_global_descriptor("electrophilicity", corrected=True)
+            return xtb.get_global_descriptor("electrophilicity", corrected=True)
+
+        except FileNotFoundError:
+            return 0.0
 
     def global_nucleophilicity(self, mol: Mol) -> float:
-        temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
-        elements, coordinates = read_xyz(geometry_path)
-        xtb = XTB(elements, coordinates)
-        self._clean_up_temp_dir(path=temp_dir)
+        try:
+            temp_dir, geometry_path = self._get_optimized_geometry_path(mol=mol)
+            elements, coordinates = read_xyz(geometry_path)
+            xtb = XTB(elements, coordinates)
+            self._clean_up_temp_dir(path=temp_dir)
 
-        return xtb.get_global_descriptor("nucleophilicity", corrected=True)
+            return xtb.get_global_descriptor("nucleophilicity", corrected=True)
+
+        except FileNotFoundError:
+            return 0.0
 
     def _get_optimized_geometry_path(self, mol: Mol):
         temp_dir = self._geometry_optimizer.optimize_xtb_geometry(mol=mol)
