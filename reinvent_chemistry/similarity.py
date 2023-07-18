e16b04b10d41fb90f8f5d3ad01ca0b784c032cee
Jenestin Antonipillai <jenestin.a@hotmail.com>
2023-07-18 15:22:55 +0200

    adding espsim calculation

--- reinvent_chemistry/similarity.py
+++ reinvent_chemistry/similarity.py
@@ -1,5 +1,7 @@
 import numpy as np
 from rdkit.Chem import DataStructs
+import rdkit.Chem as Chem
+from espsim import EmbedAlignScore
 
 
 class Similarity:
@@ -11,3 +13,22 @@ class Similarity:
         tanimoto = self.calculate_tanimoto(query_fps, ref_fingerprints)
         jaccard = 1 - tanimoto
         return jaccard
+    
+    def smiles_to_mol(self, smiles_strings):
+        mols = [Chem.rdmolops.AddHs(Chem.MolFromSmiles(smi)) for smi in smiles_strings]
+        return mols
+
+    def calc_espsim(self, ref_smi, query_smis):
+        ref = self.smiles_to_mol([ref_smi])[0] 
+        similarity_scores = []
+
+        query_mols = self.smiles_to_mol(query_smis)
+        for query in query_mols: 
+            simShape, simEsp = EmbedAlignScore(ref, query)
+            similarity_scores.append((simShape[0], simEsp[0]))
+                
+        for i, scores in enumerate(similarity_scores):
+            print(f"Scores for molecule {i+1}:")
+            print(f"simShape: {scores[0]}, simEsp: {scores[1]}")
+
+        return similarity_scores
